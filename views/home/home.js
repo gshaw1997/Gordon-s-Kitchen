122a8f2a8b6f6c8a6fd44aecf30ca1e360df5bfa
Gustavus Shaw II <gshaw1997@gmail.com>
2018-04-08 15:39:03 -0400

    Add basic home UI

--- views/home/home.js
+++ views/home/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
+import { Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 import { styles } from './home.styles';
 
 export default class HomeScreen extends React.Component {
@@ -8,9 +8,29 @@ export default class HomeScreen extends React.Component {
   };
   render() {
     return (
-      <View style={styles.container}>
-        <Text>A+ Games</Text>
-      </View>
+      <ImageBackground
+        resizeMode="cover"
+        style={styles.container}
+        source={require('../../assets/images/kitchen-bg.jpg')}
+      >
+        <View style={styles.textWrapper}>
+          <Text style={styles.title}>Welcome to the Kitchen</Text>
+        </View>
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity style={[styles.button, styles.buttonEasy]}>
+            <Text style={styles.buttonText}>Easy</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.buttonMedium]}>
+            <Text style={styles.buttonText}>Medium</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.buttonIntermediate]}>
+            <Text style={styles.buttonText}>Intermediate</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.buttonExpert]}>
+            <Text style={styles.buttonText}>Expert</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     );
   }
 }
--- views/home/home.styles.js
+++ views/home/home.styles.js
@@ -2,6 +2,47 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    height: '100%',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  textWrapper: {
+    flex: 0.75,
+    paddingVertical: 20
+  },
+  title: {
+    textAlign: 'center'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 75,
+    width: '60%',
+    borderRadius: 5,
+    marginBottom: 15
+  },
+  buttonWrapper: {
+    flex: 1.5,
+    width: '100%',
+    alignItems: 'center',
+    zIndex: 2
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#FFF'
+  },
+  buttonEasy: {
+    backgroundColor: '#78E08F'
+  },
+  buttonMedium: {
+    backgroundColor: '#F2C94C'
+  },
+  buttonIntermediate: {
+    backgroundColor: '#FA983A'
+  },
+  buttonExpert: {
+    backgroundColor: '#EB2F06'
   }
 });
